85df6b963ae651c15b98da62865cbfcec98c5aa9
decefemz <36079094+decefemz@users.noreply.github.com>
2023-12-04 21:27:08 +0200

    feat: init `poktaui` monorepo

---
+++ packages/poktaui-core/svelte.config.js
@@ -0,0 +1,17 @@
+import preprocess from 'svelte-preprocess';
+import fs from 'fs';
+export const pkg = JSON.parse(fs.readFileSync(new URL('package.json', import.meta.url), 'utf8'));
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
+  kit: {
+    files: {
+      lib: 'src'
+    }
+  }
+};
+
+export default config;
